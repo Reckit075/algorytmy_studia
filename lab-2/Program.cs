b292896680ce461623f8ede762e4064defe41111
mateusz.majka <mateusz.majka.zsl@gmail.com>
2022-03-08 10:42:13 +0100

    poprawka do 2 zadania

--- lab-2/Program.cs
+++ lab-2/Program.cs
@@ -9,7 +9,7 @@ namespace lab_2
             int[] numbers = { 1, 2, 3, 4, 5, 2 };
             int result = Sum(numbers, numbers.Length);
             Console.WriteLine(result);
-            int result2 = NumberOfAppearances(numbers, 2, numbers.Length);
+            int result2 = NumberOfAppearances(numbers, 1, numbers.Length);
             Console.WriteLine(result2);
         }
         static int Sum(int[] data, int n)
@@ -26,14 +26,14 @@ namespace lab_2
         static int NumberOfAppearances(int[] data, int index, int n)
         {
             int selectedNumber = data[index];
-            if (n <= 0 || n >= data.Length)
+            if (n <= 0 || n > data.Length)
             {
                 return 0;
             }
             else
             {
-                if (data[n] == selectedNumber) return NumberOfAppearances(data, index, n + 1) + 1;
-                else return NumberOfAppearances(data, index, n + 1);
+                if (data[n - 1] == selectedNumber) return NumberOfAppearances(data, index, n - 1) + 1;
+                else return NumberOfAppearances(data, index, n - 1);
             }
 
         }
